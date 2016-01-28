08e0bbbd4b8b2915cce3faad708abcddc6c86b98
xfdingustc <xfdingyy@qq.com>
2016-01-28 23:30:40 +0800

    add edit and update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,6 +19,21 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
   private
     def article_params
       params.require(:article).permit(:title, :text)
