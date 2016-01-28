ec9687e2ed2acce654e84b4ca16326a9796f80f1
xfdingustc <xfdingustc@gmail.com>
2016-01-27 21:32:51 -0800

    Add resource Article

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,26 @@
+class ArticlesController < ApplicationController
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def index
+    @articles = Article.all
+  end
+  private
+    def article_params
+      params.require(:article).permit(:title, :text)
+    end
+end
