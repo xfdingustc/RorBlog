f0706aa02a32c03225a578778c333453e70d4e0e
xfdingustc <xfdingyy@qq.com>
2016-01-29 10:52:53 +0800

    add comment controller

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
 
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
   root 'welcome#index'
 
 
